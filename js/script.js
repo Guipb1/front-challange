eb3f4e7c93b3d7c1834590c78542b31651c05049
Guilherme Borges <guilherme.borges@pucpr.edu.br>
2019-03-08 09:00:43 -0300

    Inclusão do estilo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
     $("#phone").mask("(00) 0000-00009");
-});
+
+    }
+} );
+
+function checkPassword() {
+        var password = $("#password").val();
+        var confirmPassword = $("#repeat").val();
+
+        if (password != confirmPassword)
+            alert("ok");
+        else
+            alert("erro nao igual");
+    }
